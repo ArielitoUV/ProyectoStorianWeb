b32f77262fd6e7b8dc725034bf469c46d6dacd31
Antho <antho@gmail.com>
2023-11-03 15:56:21 -0300

    agregar views

--- StorianWeb/core/views.py
+++ StorianWeb/core/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "core/index.html")
+
+def registro(request):
+    return render(request, "core/registro.html")
+
+def iniciosesion(request):
+    return render(request, "core/iniciosesion.html")
+
+def busqueda(request):
+    return render(request, "core/busqueda.html")
+
+def viajetiempo(request):
+    return render(request, "core/viajetiempo.html")
+
+def reseñas(request):
+    return render(request, "core/reseñas.html")
+
+def planificar(request):
+    return render(request, "core/planificar.html")
+
+def formulariovisita(request):
+    return render(request, "core/formulariovisita.html")
+
+def informacion(request):
+    return render(request, "core/informacion.html")
+
+def usuarios(request):
+    return render(request, "core/usuarios.html")
